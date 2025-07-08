b404c5703c148ae7af8e5b3acec567984fb1f679
PricelessToolkit <81021972+PricelessToolkit@users.noreply.github.com>
2025-07-08 18:39:35 +0200

    Update config.h

--- Code/PirBox-Lite/config.h
+++ Code/PirBox-Lite/config.h
@@ -25,13 +25,18 @@
 
 #define BAND                      868E6     // 433E6 MHz or 868E6 MHz or 915E6 MHz
 #define TX_OUTPUT_POWER           20        // dBm tx output power
-#define LORA_BANDWIDTH            4         // bandwidth 2: 31.25Khz, 3: 62.5Khz, 4: 125Khz, 5: 250KHZ, 6: 500Khz
+#define LORA_BANDWIDTH            4         // bandwidth 4: 125Khz, 5: 250KHZ, 6: 500Khz
 #define LORA_SPREADING_FACTOR     8         // spreading factor 6-12 [SF5..SF12]
 #define LORA_CODINGRATE           1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
 #define LORA_PREAMBLE_LENGTH      6         // Same for Tx and Rx
 #define LORA_PAYLOADLENGTH        0         // 0: Variable length packet (explicit header),  1..255 for Fixed length packet (implicit header)
 #define LORA_CRC_ON               true
 #define LORA_SYNC_WORD            0x1424    // The 0x1424 private sync word is equivalent to the CapiBridge 0x12 sync word.
+
+// There are the following restrictions:
+// LORA_BANDWIDTH 125KHz, SF is in the range of 5-9.
+// LORA_BANDWIDTH 250KHz, SF is in the range of 5-10.
+// LORA_BANDWIDTH 500KHz, SF is in the range of 5-11.
 
 // LoRa Sync Word Compatibility Table
 //
